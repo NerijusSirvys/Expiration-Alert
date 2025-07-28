36423c9e8cf21d2ebc0bca94b50036fa02aa9ecb
Nerijus Sirvys <n.sirvys@senso.cloud>
2025-07-28 19:21:51 +0300

    added navigation and colors

--- src/Android/app/src/main/java/com/ns/expiration/expiration/alert/MainActivity.kt
+++ src/Android/app/src/main/java/com/ns/expiration/expiration/alert/MainActivity.kt
@@ -7,10 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ns.expiration.expiration.alert.navigation.Destinations
 import com.ns.expiration.expiration.alert.ui.theme.ExpirationAlertTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,14 @@ class MainActivity : ComponentActivity() {
       enableEdgeToEdge()
       setContent {
          ExpirationAlertTheme {
+            val navController = rememberNavController()
             Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-               Greeting(
-                  name = "Android",
-                  modifier = Modifier.padding(innerPadding)
-               )
+               NavHost(modifier = Modifier.padding(innerPadding), navController = navController, startDestination = Destinations.Home) {
+                  composable<Destinations.Home> { }
+                  composable<Destinations.NewAlert> { }
+               }
             }
          }
       }
-   }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-   Text(
-      text = "Hello $name!",
-      modifier = modifier
-   )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-   ExpirationAlertTheme {
-      Greeting("Android")
    }
 }
