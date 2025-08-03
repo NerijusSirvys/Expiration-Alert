ac4a3f443f1facb23f39ede8efabf11d9d2416a1
Nerijus Sirvys <n.sirvys@senso.cloud>
2025-08-03 20:38:27 +0300

    added slim text field

---
+++ src/Android/app/src/main/java/com/ns/expiration/expiration/alert/screens/home/components/textField/TextFieldIcon.kt
@@ -0,0 +1,8 @@
+package com.ns.expiration.expiration.alert.screens.home.components.textField
+
+import androidx.compose.ui.graphics.painter.Painter
+
+data class TextFieldIcon(
+   val painter: Painter,
+   val contentDescription: String
+)
