58e2233af6d4eb32acdcfe37a64d74e66c6e4cec
Nerijus Sirvys <n.sirvys@senso.cloud>
2025-08-20 22:51:49 +0300

    updated colors

--- src/Android/app/src/main/java/com/ns/expiration/expiration/alert/ui/theme/Color.kt
+++ src/Android/app/src/main/java/com/ns/expiration/expiration/alert/ui/theme/Color.kt
@@ -10,8 +10,9 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val Background = Color(0XFF21252A)
+
 val White = Color(0XFFFFFFFF)
-val CharcoalGrey = Color(0XFF21252A)
 val SlateGrey = Color(0XFF353B40)
 val SunRay = Color(0XFFFEB65A)
 val RedSalsa = Color(0XFFF85252)
