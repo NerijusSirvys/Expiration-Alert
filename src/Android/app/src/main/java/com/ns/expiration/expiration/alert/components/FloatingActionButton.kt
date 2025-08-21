b6b3830f5ba7da4b9069486f67e227b16b9cfe19
Nerijus Sirvys <n.sirvys@senso.cloud>
2025-08-21 20:02:32 +0300

    Moved FloatingActionButton to components package and updated color

--- src/Android/app/src/main/java/com/ns/expiration/expiration/alert/components/FloatingActionButton.kt
+++ src/Android/app/src/main/java/com/ns/expiration/expiration/alert/components/FloatingActionButton.kt
@@ -1,4 +1,4 @@
-package com.ns.expiration.expiration.alert.screens.home.components
+package com.ns.expiration.expiration.alert.components
 
 import android.content.res.Configuration
 import androidx.compose.material3.ExtendedFloatingActionButton
@@ -9,7 +9,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.ns.expiration.expiration.alert.ui.theme.Background
 import com.ns.expiration.expiration.alert.ui.theme.ExpirationAlertTheme
-import com.ns.expiration.expiration.alert.ui.theme.SunRay
+import com.ns.expiration.expiration.alert.ui.theme.Primary
 
 @Composable
 fun FloatingActionButton(
@@ -20,7 +20,7 @@ fun FloatingActionButton(
    ExtendedFloatingActionButton(
       modifier = modifier,
       onClick = onClick,
-      containerColor = SunRay,
+      containerColor = Primary,
       contentColor = Background
    ) {
       Text(text = text)
