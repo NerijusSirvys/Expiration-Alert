2a6109f002ace7c3bab698f6ecacd5cc10305bed
Nerijus Sirvys <n.sirvys@senso.cloud>
2025-08-24 17:31:50 +0300

    add border radius to alert details image

--- src/Android/app/src/main/java/com/ns/expiration/expiration/alert/screens/details/AlertDetailsScreenContent.kt
+++ src/Android/app/src/main/java/com/ns/expiration/expiration/alert/screens/details/AlertDetailsScreenContent.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -98,7 +99,9 @@ fun AlertDetailsScreenContent(
 
          InformationCard(modifier = modifier, label = "Image") {
             SubcomposeAsyncImage(
-               modifier = modifier.fillMaxWidth(),
+               modifier = modifier
+                  .fillMaxWidth()
+                  .clip(MaterialTheme.shapes.extraSmall),
                model = state.data.imageUrl,
                clipToBounds = true,
                contentScale = ContentScale.FillWidth,
